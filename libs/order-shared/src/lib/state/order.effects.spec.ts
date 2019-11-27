76b847a5723a785f4b42f7f9c7c2be4479680bc5
Marvin Luchs <marvin.luchs@holisticon.de>
2019-11-27 11:24:48 +0100

    Add test for OrderEffects

--- libs/order-shared/src/lib/state/order.effects.spec.ts
+++ libs/order-shared/src/lib/state/order.effects.spec.ts
@@ -3,20 +3,40 @@ import { provideMockActions } from '@ngrx/effects/testing';
 import { provideMockStore } from '@ngrx/store/testing';
 import { Observable } from 'rxjs';
 import { OrderEffects } from './order.effects';
+import { hot } from 'jest-marbles';
+import { checkoutOrder, checkoutOrderSuccess } from './order.actions';
+import { TestScheduler } from 'rxjs/internal/testing/TestScheduler';
 
 describe('OrderEffects', () => {
     let actions: Observable<any>;
     let effects: OrderEffects;
+    let testScheduler: TestScheduler;
 
     beforeEach(() => {
+        testScheduler = new TestScheduler((actual, expected) => {
+            expect(actual).toEqual(expected);
+        });
+
         TestBed.configureTestingModule({
             providers: [
                 OrderEffects,
                 provideMockActions(() => actions),
-                provideMockStore()
             ]
         });
 
         effects = TestBed.get(OrderEffects);
+    });
+
+    describe('checkoutOrder', () => {
+        it('returns checkoutOrderSuccess after 2 seconds', () => {
+            testScheduler.run(({ cold, expectObservable }) => {
+                    actions = cold('a', { a: checkoutOrder() });
+
+                expectObservable(effects.checkoutOrder).toBe(
+                    '2s a',
+                    { a: checkoutOrderSuccess() }
+                )
+            });
+        });
     });
 });
