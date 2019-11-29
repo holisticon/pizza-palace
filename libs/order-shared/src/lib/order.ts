e0ad094e521b14e4db1095aa704cdc575dddbe95
Marvin Luchs <marvin.luchs@holisticon.de>
2019-11-29 18:50:23 +0100

    Remove used function

--- libs/order-shared/src/lib/order.ts
+++
@@ -1,8 +0,0 @@
-import { OrderItem } from './order.model';
-
-export function toTotalPrice(items: OrderItem[]) {
-    return items.reduce(
-        (total, item) => total + item.pizza.price,
-        0
-    );
-}
